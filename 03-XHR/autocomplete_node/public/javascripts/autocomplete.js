b24a405b85f98099108ecb3b8d951b430879b555
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-08-24 14:06:48 +0200

    autocomplete ui creation

--- 03-XHR/autocomplete_node/public/javascripts/autocomplete.js
+++ 03-XHR/autocomplete_node/public/javascripts/autocomplete.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const Autocomplete = {
   init() {
     this.input = document.querySelector("input");
@@ -5,9 +6,13 @@ const Autocomplete = {
 
     this.listUI = null;
     this.overlay = null;
+    this.bestMatchIndex = null;
 
     this.wrapInput();
     this.createUI();
+    this.bindEvents();
+
+    this.reset();
   },
 
   wrapInput() {
@@ -28,6 +33,76 @@ const Autocomplete = {
     overlay.style.width = `${this.input.clientWidth}px`;
     this.input.parentNode.appendChild(overlay);
     this.overlay = overlay;
+  },
+
+  bindEvents() {
+    this.input.addEventListener("input", this.valueChanged.bind(this));
+  },
+
+  valueChanged() {
+    let value = this.input.value;
+
+    if (value.length > 0) {
+      this.fetchMatches(value, matches => {
+        this.visible = true;
+        this.matches = matches;
+        this.bestMatchIndex = 0;
+        this.draw();
+      });
+    } else {
+      this.reset();
+    }
+  },
+
+  fetchMatches(query, callback) {
+    let request = new XMLHttpRequest();
+
+    request.addEventListener("load", () => {
+      callback(request.response);
+    });
+
+    request.open("GET", `${this.url}${encodeURIComponent(query)}`);
+    request.responseType = "json";
+    request.send();
+  },
+
+  draw() {
+    while (this.listUI.lastChild) {
+      this.listUI.removeChild(this.listUI.lastChild);
+    }
+
+    if (!this.visible) {
+      this.overlay.textContent = "";
+      return;
+    }
+
+    if (this.bestMatchIndex !== null && this.matches.length > 0) {
+      let selected = this.matches[this.bestMatchIndex];
+      this.overlay.textContent =
+        this.generateOverlayContent(this.input.value, selected);
+    } else {
+      this.overlay.textContent = "";
+    }
+
+    this.matches.forEach(match => {
+      let li = document.createElement("li");
+      li.classList.add("autocomplete-ui-choice");
+      li.textContent = match.name;
+      this.listUI.appendChild(li);
+    });
+  },
+
+  generateOverlayContent(value, match) {
+    let end = match.name.substr(value.length);
+    return value + end;
+  },
+
+  reset() {
+    this.visible = false;
+    this.matches = [];
+    this.bestMatchIndex = null;
+
+    this.draw();
   }
 };
 
